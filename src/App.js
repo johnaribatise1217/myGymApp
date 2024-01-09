f65d93a16543bacf64a59ad22570772b4dc788ff
John Aribatise <johnaribatise@hsass.org>
2024-01-09 03:00:13 +0100

    updated web

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Contact from './Pages/Contact/Contact'
 import Gallery from './Pages/Gallery/Gallery'
 import Plans from './Pages/Plans/Plans'
 import Trainers from './Pages/Trainers/Trainers'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
 import aboutImg from './Images/header3.jpg'
 import SignUp from './SignUp/Signup'
@@ -12,20 +12,22 @@ import Footer from './Footer/Footer'
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Routes>
-        <Route index path='/' element={<Home/>}/>
-        <Route path='about' element={<About img={aboutImg}/>}/>
-        <Route path='contact' element={<Contact/>}/>
-        <Route path='gallery' element={<Gallery/>}/>
-        <Route path='plans' element={<Plans/>}/>
-        <Route path='trainers' element={<Trainers/>}/>
-        {/* <Route path='*' element={<NotFound/>}/> */}
-        <Route path='/signup' element={<SignUp/>}/>
-      </Routes>
-      <Footer/>
-    </>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div>
+        <Navbar/>
+        <Routes>
+          <Route index path='/' element={<Home/>}/>
+          <Route path='about' element={<About img={aboutImg}/>}/>
+          <Route path='contact' element={<Contact/>}/>
+          <Route path='gallery' element={<Gallery/>}/>
+          <Route path='plans' element={<Plans/>}/>
+          <Route path='trainers' element={<Trainers/>}/>
+          {/* <Route path='*' element={<NotFound/>}/> */}
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
